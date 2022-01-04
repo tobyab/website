fd24b2787306e3af93ef57dabce86c8b9b18f42e
ItsTobez <mrtobez08@gmail.com>
2022-01-04 10:11:10 +0000

    changes made

--- src/components/cmdk.js
+++ src/components/cmdk.js
@@ -1,75 +1,43 @@
 import {
-    KBarResults,
-    useMatches,
-    NO_GROUP,
-  } from "kbar";
-  // app.tsx
-import {
     KBarProvider,
     KBarPortal,
     KBarPositioner,
     KBarAnimator,
     KBarSearch,
+    KBarResults,
+    useMatches,
+    NO_GROUP,
   } from "kbar";
 
-function MyApp() {
-  return 
-  <KBarProvider>
+function CommandK() {
+     const actions = [{
+          id: "blog",
+          name: "Blog",
+          shortcut: ["b"],
+          keywords: "writing words",
+          perform: () => (window.location.pathname = "blog"),
+        },
+        {
+          id: "contact",
+          name: "Contact",
+          shortcut: ["c"],
+          keywords: "email",
+          perform: () => (window.location.pathname = "contact"),
+        },
+        // space for more actions
+      ];
       
-      </KBarProvider>;
-}
-  
-  const actions = [
-    {
-      id: "blog",
-      name: "Blog",
-      shortcut: ["b"],
-      keywords: "writing words",
-      perform: () => (window.location.pathname = "blog"),
-    },
-    {
-      id: "contact",
-      name: "Contact",
-      shortcut: ["c"],
-      keywords: "email",
-      perform: () => (window.location.pathname = "contact"),
-    },
-  ]
-
-  
-  function RenderResults() {
-    const { results } = useMatches();
-  
     return (
-      <KBarResults
-        items={results}
-        onRender={({ item, active }) =>
-          typeof item === "string" ? (
-            <div>{item}</div>
-          ) : (
-            <div
-              style={{
-                background: active ? "#eee" : "transparent",
-              }}
-            >
-              {item.name}
-              <KBarProvider actions={actions}>
-               <KBarPortal>, // Renders the content outside the root node
-                 <KBarPositioner>, // Centers the content
-                   <KBarAnimator>, // Handles the show/hide and height animations
-                     <KBarSearch />, // Search input
-                   </KBarAnimator>,
-                 </KBarPositioner>,
-               </KBarPortal>,
-               <MyApp />,
-             </KBarProvider>
-             <RenderResults />;
-             <KBarProvider actions={actions}>
-
+  <KBarProvider actions={actions}>
+          <KBarProvider actions={actions}>
+      <KBarPortal>
+        <KBarPositioner> 
+          <KBarAnimator> 
+            <KBarSearch /> 
+          </KBarAnimator>
+        </KBarPositioner>
+      </KBarPortal>
+      <CommandK />
+    </KBarProvider>;
     </KBarProvider>
-            </div>
-          )
-        }
-      />
-    );
-  }
+)};
