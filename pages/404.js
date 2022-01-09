e39f6211ea6bfb6d9604b708e8a7eed6e8650f30
ItsTobez <mrtobez08@gmail.com>
2022-01-09 11:55:51 +0000

    added 404.js

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Error.module.css'
+
+export default function notFound() {
+    return ( 
+        <div className={styles.container}>
+            <Head>
+                <title>Toby B / 404</title>
+                <link rel="icon" href="/peep (3).png" />
+                </Head>
+                <body>
+                    <h1 className={styles.title}>404</h1>
+                    <h3 className={styles.subtitle}>Gadzooks! The page you were searching for wasn&apos;t found!</h3>
+                    <button onClick={retHome} className={styles.return}>Back to the landing!</button>
+                </body>
+                </div>
+     );
+}
+
+function retHome() {
+    window.open("/", "_self");
+}
