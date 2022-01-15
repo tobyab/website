771fc14486db33b6ad341ed0c11876380e8137d0
ItsTobez <mrtobez08@gmail.com>
2022-01-15 10:20:39 +0000

    changes made

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,11 @@ import {
   KBarResults,
   useMatches,
 } from "kbar";
-import { useRouter } from "next/router"; 
+import { useRouter } from "next/router";
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-12341234-1";
+ReactGA.initialize(TRACKING_ID);
 
 const searchStyle = {
   padding: "12px 16px",
@@ -206,6 +210,28 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+function HomeIcon() {
+  return (
+    <svg width="100" height="100" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m19.681 10.406-7.09-6.179a.924.924 0 0 0-1.214.002l-7.06 6.179c-.642.561-.244 1.618.608 1.618.51 0 .924.414.924.924v5.395c0 .51.414.923.923.923h3.236V14.54c0-.289.234-.522.522-.522h2.94c.288 0 .522.233.522.522v4.728h3.073c.51 0 .924-.413.924-.923V12.95c0-.51.413-.924.923-.924h.163c.853 0 1.25-1.059.606-1.62Z"
+        fill="var(--foreground)"
+      />
+    </svg>
+  );
+}
+
+function cmdIcon() {
+  return (
+  <svg onClick={useKBar} width="100" height="100" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m19.681 10.406-7.09-6.179a.924.924 0 0 0-1.214.002l-7.06 6.179c-.642.561-.244 1.618.608 1.618.51 0 .924.414.924.924v5.395c0 .51.414.923.923.923h3.236V14.54c0-.289.234-.522.522-.522h2.94c.288 0 .522.233.522.522v4.728h3.073c.51 0 .924-.413.924-.923V12.95c0-.51.413-.924.923-.924h.163c.853 0 1.25-1.059.606-1.62Z"
+        fill="#000"
+      />
+    </svg>
+  );
+}
 
 // MASSIVE thank you to Ella for helping me with kbar,
 // I strongly recommend you check out her github here: https://github.com/eilla1 :)
