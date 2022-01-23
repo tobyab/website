2d47423dfd574e2de601e5574fd0be4e7cb49a4f
ItsTobez <mrtobez08@gmail.com>
2022-01-23 11:10:56 +0000

    PS on proj page styled

--- pages/projects.js
+++ pages/projects.js
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import Head from 'next/head'
 import { useKBar } from 'kbar'
 
-
 export default function Projects() {
   const { query } = useKBar();
   return (
@@ -27,11 +26,11 @@ export default function Projects() {
     <body>
 
     <h1 className={styles.title}>Projects.</h1>
-    <h1 className={styles.subtitle}> To see all of my projects, check out my <a className={styles.gitRef} onClick={githubRef}>GitHub</a>!</h1>
+    <h1 className={styles.subtitle}> To see all of my projects, check out my <a className={styles.gitRef} onClick={githubRef}>GitHub</a>.</h1>
 
     <div className={styles.homeadvisorContatiner}>
       <div className={styles.HAwrapper}>
-    <Image className={styles.HAimg} src="/homeAdv.svg" alt="Home advisor" width="100%" height="100%" layout="responsive" objectFit="contain" priority />
+    <Image className={styles.HAimg} src="/HA-prev.svg" alt="Home advisor" width="100%" height="100%" layout="responsive" objectFit="contain" priority />
     </div>
       <h1 className={styles.HAtitle}>Home advisor</h1>
       <p className={styles.HAdesc}>Home advisor is the perfect way <br />
@@ -45,11 +44,12 @@ export default function Projects() {
     <div className={styles.posstartContatiner}>
     <div className={styles.PSwrapper}>
 
-    <Image className={styles.PSimg} src="/PS.svg" alt="Positive Start" width="100%" height="100%" layout="responsive" objectFit="contain" />
+    <Image className={styles.PSimg} src="/PS-prev.svg" alt="Positive Start" width="100%" height="100%" layout="responsive" objectFit="contain" />
 
     </div>
       <h1 className={styles.PStitle}>Positive start</h1>
       <p className={styles.PSdesc}>Positive start is an email newsletter that <br /> helps you look fowards to waking up.</p>
+      <a className={styles.PSref} onClick={showPS}>Check it out</a>
     </div>
 </body>
 </div>
@@ -58,4 +58,8 @@ export default function Projects() {
 
 function githubRef() {
   window.open("https://github.com/itstobez?tab=repositories", "_blank");
+}
+
+function showPS() {
+  window.open("https://positivestart.vercel.app", "_blank");
 }
