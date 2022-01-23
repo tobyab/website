d3deedce198351dd70171d9e9758e6d312922275
ItsTobez <mrtobez08@gmail.com>
2022-01-23 12:13:12 +0000

    projects page optimized for mobile

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const { query } = useKBar();
           property="og:description"
           content="13 y/o full stack web dev & designer."
         />
-        <meta property="og:image" content={"../public/preview.svg"} />
+        <meta property="og:image" content={"/preview.svg"} />
       </Head>
       <body>
       <ChakraProvider>
