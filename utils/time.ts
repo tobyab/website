5f0aee0d55c6ee6b1b80ffdbead00ca9b2966fdb
Toby Brown <hi@tobyb.dev>
2023-07-25 17:48:24 -0400

    updating time.ts

--- utils/time.ts
+++ utils/time.ts
@@ -1,13 +1,3 @@
-var birthday = new Date("2008/11/26");
-var diff = new Date().valueOf() - birthday.valueOf();
-export var age = Math.floor(diff / 31536000000);
-
-export var year = new Date().getFullYear();
-
-export var dayTime = new Date();
-var currentHour = dayTime.getHours();
-
-export var message;
-if (currentHour < 12) message = "morning";
-else if (currentHour >= 12 && currentHour <= 17) message = "afternoon";
-else if (currentHour >= 17 && currentHour <= 24) message = "evening";
+let birthday = new Date("2008/11/26");
+let diff = new Date().valueOf() - birthday.valueOf();
+export let age = Math.floor(diff / 31536000000);
