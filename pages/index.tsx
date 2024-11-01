55ca4addef9b851ba04d470a0c00dcafad24905c
Toby Brown <hi@tobyb.dev>
2024-11-01 23:33:54 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({ data }) {
             Toby Brown is a <>{age}</> year old full-stack web developer &
             designer in London{" "}
             <span className="text-darkGrey">
-              — currently making a thinking machine at Beem.
+              — currently creating the next generation of computing at Beem.
             </span>
           </H1>
           <div className="grid sm:grid-cols-2 sm:mt-24 mt-12 sm:space-x-8 sm:space-y-0 space-y-8 text-darkGrey">
