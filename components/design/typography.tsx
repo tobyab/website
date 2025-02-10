a453db51bb301ecb1f6b67fb6ddfb5def40d434e
Toby Brown <hi@tobyb.dev>
2025-02-10 11:14:14 +0000

    fixes less rubbish

--- components/design/typography.tsx
+++ components/design/typography.tsx
@@ -1,13 +1,7 @@
-export function H1({ className, children }: any) {
-  return <h1 className={`text-3xl ${className}`}>{children}</h1>;
-}
-
 export function H2({ className, children }: any) {
-  return <h2 className={`text-[1.625rem] font-light ${className}`}>{children}</h2>;
-}
-
-export function P({ className, children }: any) {
-  return <p className={`text-lg ${className}`}>{children}</p>;
+  return (
+    <h2 className={`text-[1.625rem] font-light ${className}`}>{children}</h2>
+  );
 }
 
 export function S({ className, children }: any) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,64 +8,35 @@ export default function Home() {
       <div className="max-w-2xl space-y-4 m-12">
         <H2>
           Toby Brown is an inventor in London. He is the founder of a company
-          called{" "}
-          <Link
-            href="https://beem.computer"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
-            Beem
-          </Link>
-          . Their mission is to define how people will be interacting with their
-          computers in the future.
+          called <Np href="https://beem.computer">Beem</Np>. Their mission is to
+          define how people will be interacting with their computers in the
+          future.
         </H2>
         <H2>
           Toby thinks a lot about computer interaction, and how we can make our
           computers more human. He&apos;s spoken about this{" "}
-          <Link
-            href="https://www.youtube.com/watch?v=dHkD91vYau8"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
-            on TV
-          </Link>
-          , and{" "}
-          <Link
-            href="https://www.thetimes.com/business-money/entrepreneurs/article/the-16-year-old-swapping-gcses-for-silicon-valley-and-his-1m-start-up-00cptjxqh"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
+          <Np href="https://www.youtube.com/watch?v=dHkD91vYau8">on TV</Np>, and{" "}
+          <Np href="https://www.thetimes.com/business-money/entrepreneurs/article/the-16-year-old-swapping-gcses-for-silicon-valley-and-his-1m-start-up-00cptjxqh">
             in the papers
-          </Link>
+          </Np>
           .
         </H2>
         <H2>
           Toby&apos;s most active on{" "}
-          <Link
-            href="https://x.com/developedbytoby"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
-            X
-          </Link>
-          . Here&apos;s his{" "}
-          <Link
-            href="https://read.cv/tobyab"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
-            CV
-          </Link>
-          . If you want to chat, you can email him at{" "}
-          <Link
-            href="mailto:toby@beem.computer"
-            className="underline underline-offset-2"
-            target="_blank"
-          >
-            toby@beem.computer
-          </Link>
+          <Np href="https://x.com/developedbytoby">X</Np>. Here&apos;s his{" "}
+          <Np href="https://read.cv/tobyab">CV</Np>. If you want to chat, you
+          can email him at{" "}
+          <Np href="mailto:toby@beem.computer">toby@beem.computer</Np>
         </H2>
       </div>
     </div>
+  );
+}
+
+function Np({ href, children }: { href: string; children: React.ReactNode }) {
+  return (
+    <Link href={href} className="underline underline-offset-2" target="_blank">
+      {children}
+    </Link>
   );
 }
